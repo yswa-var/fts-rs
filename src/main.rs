07e690ea4d00d035844c968209919c9607c5536b
yswa-var <yswa.var@gmail.com>
2026-09-01 12:57:29 +0530

    totp generation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::env;
 
+use totp_rs::{Algorithm, Builder, Secret};
+
 #[derive(Debug)]
 struct Config {
     client_id: String,
-    totp: String,
+    totp_key: String,
     dpin: String,
 }
 
@@ -13,9 +15,21 @@ impl Config {
 
         Self {
             client_id: env::var("CLIENT_ID").expect("CLIENT_ID is missing"),
-            totp: env::var("TOTP_KEY").expect("TOTP_KEY is missing"),
+            totp_key: env::var("TOTP_KEY").expect("TOTP_KEY is missing"),
             dpin: env::var("DPIN").expect("DPIN is missing"),
         }
+    }
+
+    fn current_totp(&self) -> String {
+        let secret = Secret::try_from_base32(&self.totp_key).expect("Invalid TOTP secret");
+
+        let totp = Builder::new()
+            .with_secret(secret)
+            .with_algorithm(Algorithm::SHA1)
+            .build()
+            .expect("Failed to build TOTP");
+
+        totp.generate_current().to_string()
     }
 }
 
@@ -23,4 +37,8 @@ fn main() {
     let config = Config::from_env();
 
     println!("{:#?}", config);
+
+    let totp = config.current_totp();
+
+    println!("TOTP: {}", totp)
 }
